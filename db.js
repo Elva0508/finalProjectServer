7e39204b745c2835b5800a9d6d75549d4f21f408
Elva0508 <idawg071010wk@gmail.com>
2023-10-24 00:40:19 +0800

    npm i crypto

--- db.js
+++ db.js
@@ -1,14 +1,7 @@
 const mysql = require("mysql2");
-const pool = mysql.createPool({
-  connectionLimit: 10, // 最大连接数
-  host: "/cloudsql/numeric-marker-402719:asia-east1:final-project",
-  port: 3306,
-  user: "root",
-  password: "12345",
-  database: "cat",
-});
+
 const connection = mysql.createConnection({
-  host: "35.221.205.131",
+  host: "/cloudsql/numeric-marker-402719:asia-east1:final-project",
   port: 3306,
   user: "root",
   password: "12345",
